76909ed35d951adcf8013f422af84949363b4b7f
CallmePapa <1062927638@qq.com>
2017-12-18 20:27:30 +0800

    :sparkles: cart&&list

--- src/service/cart-service.js
+++ src/service/cart-service.js
@@ -1,3 +1,81 @@
 /**
  * Created by weiqiujuan on 17-12-8.
  */
+let _mm=require('util/mall.js');
+let _cart={
+    getCartCount:function (resolve,reject) {
+        _mm.request({
+            url:_mm.getServerUrl('/cart/get_cart_product_count.do'),
+            success:resolve,
+            error:reject
+        });
+    },
+    //添加到购物车
+    addToCart:function (productInfo,resolve,reject) {
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/add.do'),
+            data    : productInfo,
+            success : resolve,
+            error   : reject
+        });
+    },
+    //获取购物车列表
+    getCartList:function (resolve,reject) {
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/list.do'),
+            success : resolve,
+            error   : reject
+        })
+    },
+    //选择购物车商品
+    selectProduct:function (productId,resolve,reject) {
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/select.do'),
+            data    : {
+                productId : productId
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    //取消选择购物车商品
+    unselectProduct : function(productId, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/un_select.do'),
+            data    : {
+                productId : productId
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    //选中全部商品
+    selectAllProduct:function (resolve,reject) {
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/un_select_all.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 更新购物车商品数量
+    updateProduct : function(productInfo, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/update.do'),
+            data    : productInfo,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 删除指定商品
+    deleteProduct : function(productIds, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/delete_product.do'),
+            data    : {
+                productIds : productIds
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+};
+module.exports=_cart;
---
+++ src/util/pagination/index.js
@@ -0,0 +1,78 @@
+/**
+ * Created by weiqiujuan on 17-12-18.
+ */
+require('./index.css');
+
+let _mm = require('util/mall.js');
+/*
+ let templatePagination=require('./index.string');*/
+
+let Pagination = function () {
+    let _this = this;
+    this.defaultOption = {
+        container: null,
+        pageNum: 1,
+        pageRange: 3,
+        onSelectPage: null
+    };
+    //事件处理
+    $(document).on('click', '.pg-item', function () {
+        let $this = $(this);
+        //对于active和disabled按钮点击，不做处理
+        if ($this.hasClass('active') || $this.hasClass('disabled')) {
+            return;
+        }
+        typeof _this.option.onSelectPage === 'function' ? _this.option.onSelectPage($this.data('value')) : null;
+    })
+};
+//渲染分页组建
+Pagination.prototype.render = function (userOption) {
+    //合并选项
+    this.option = $.extend({}, this.defaultOption, userOption);
+    //判断容器是否为合法的jquery对象
+    if (!(this.option.container instanceof jQuery)) {
+        return;
+    }
+    //判断是否只有一页
+    if (this.option.pages <= 1) {
+        return;
+    }
+    this.option.container.html(this.getPaginationHtml());
+};
+//获取分页的html
+Pagination.prototype.getPaginationHtml = function () {
+    let html = '',
+        option = this.option,
+        pageArray = [],
+        start = option.pageNum - option.pageRange > 0
+            ? option.pageNum - option.pageRange : 1,
+        end = option.pageNum + page.pageRange < option.pages
+            ? option.pageNum + option.pageRange : option.pages;
+    //上一页按钮的数据
+    pageArray.push({
+        name: '上一页',
+        value: this.option.prePage,
+        disabled: !this.option.hasPreviousPage
+    });
+    //数字按钮的处理
+    for (let i = start; i <= end; i++) {
+        pageArray.push({
+            name: i,
+            value: i,
+            active: (i === option.pageNum)
+        });
+    }
+    //下一页按钮的数据
+    pageArray.push({
+        name: '下一页',
+        value: this.option.nextPage,
+        disabled:!this.option.hasNextPage
+    });
+    html=_mm.renderHtml(templatePagination,{
+        pageArray:pageArray,
+        pageNum:option.pageNum,
+        pages:option.pages
+    });
+    return html;
+};
+module.exports=Pagination;
