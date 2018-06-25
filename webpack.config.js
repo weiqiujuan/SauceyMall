4a7541fb859a0d582329ada9f46dd01b89131f5c
CallmePapa <1062927638@qq.com>
2018-06-25 14:40:06 +0800

    修改配置文件将webpack1升级到webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,182 @@
 var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path=require('path');
 
-//环境变量的配置.dev/online
+// 环境变量配置，dev / online
 var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
-
 
 //获取html-webpack-plugin参数的方法
 var getHtmlConfig = function (name, title) {
     return {
-        template: "./src/view/" + name + ".html",
-        filename: "view/" + name + ".html",
+        template: './src/view/' + name + '.html',
+        filename: 'view/' + name + '.html',
+        favicon: './favicon.ico',
         title: title,
         inject: true,
         hash: true,
-        chunks: ["common", name]
+        chunks: ['common', name]
     };
 };
 
 //webpack config
 var config = {
+
+    //【新增】新增mode参数，webpack4中要指定模式，可以放在配置文件这里，
+    // 也可以放在启动命令里，如--mode production
+    mode: 'dev' === WEBPACK_ENV ? 'development' : 'production',
+
+    //【改动】：删除了入口文件的中括号，可选的改动，没什么影响
     entry: {
-        "common": ['./src/page/common/index.js'],
-        "index": ['./src/page/index/index.js'],
-        'detail': ['./src/page/detail/index.js'],
-        'list': ['./src/page/list/index.js'],
-        'cart': ['./src/page/cart/index.js'],
-        'user-login': ['./src/page/user-login/index.js'],
-        'user-register': ['./src/page/user-register/index.js'],
-        'user-pass-reset': ['./src/page/user-pass-reset/index.js'],
-        'user-center': ['./src/page/user-center/index.js'],
-        'user-center-update': ['./src/page/user-center-update/index.js'],
-        'user-pass-update': ['./src/page/user-pass-update/index.js'],
-        'result': ['./src/page/result/index.js']
+        'common': './src/page/common/index.js',
+        'index': './src/page/index/index.js',
+        'list': './src/page/list/index.js',
+        'detail': './src/page/detail/index.js',
+        'cart': './src/page/cart/index.js',
+        'order-confirm': './src/page/order-confirm/index.js',
+        'order-list': './src/page/order-list/index.js',
+        'order-detail': './src/page/order-detail/index.js',
+        'payment': './src/page/payment/index.js',
+        'user-login': './src/page/user-login/index.js',
+        'user-register': './src/page/user-register/index.js',
+        'user-pass-reset': './src/page/user-pass-reset/index.js',
+        'user-center': './src/page/user-center/index.js',
+        'user-center-update': './src/page/user-center-update/index.js',
+        'user-pass-update': './src/page/user-pass-update/index.js',
+        'result': './src/page/result/index.js',
+        'about': './src/page/about/index.js'
     },
     output: {
-        path: './dist',//存放路径
-        publicPath: '/dist',//访问路径
+        //  【改动】：删除path的配置，在webpack4中文件默认生成的位置就是/dist,
+        // 而publicPath和filename特性的设置要保留
+        //path:__dirname+'/dist/'
+
+        publicPath: 'dev' === WEBPACK_ENV ? '/dist/' : '//s.happymmall.com/mmall-fe/dist/',
         filename: 'js/[name].js'
     },
     externals: {
-        "jquery": "window.jQuery"
+        'jquery': 'window.jQuery'
     },
     module: {
-        loaders: [
-            {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
-            {test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]'},
-            {test: /\.string$/, loader: 'html-loader'}
+        // 【改动】：loader的使用中，loaders字段变为rules，
+        // 用来放各种文件的加载器，用rules确实更为贴切
+        rules: [
+            // 【改动】：css样式的加载方式变化
+            // css文件的处理
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader", use: "css-loader"
+                })
+            },
+
+            // 【改动】：模板文件的加载方式变化
+            // 模板文件的处理
+            {
+                test: /\.string$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true, removeAttributeQuotes: false
+                    }
+                }
+            },
+
+            // 【改动】：图片文件的加载方式变化，并和字体文件分开处理
+            // 图片的配置
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            //【改动】：图片小于2kb的按base64打包
+                            limit: 2048,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+
+            // 【改动】：字体文件的加载方式变化
+            // 字体图标的配置
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: 'resource/[name].[ext]'
+                    }
+                }]
+            }
         ]
     },
     resolve: {
         alias: {
-            node_modules: __dirname + "/node_modules",
-            util: __dirname + "/src/util",
-            page: __dirname + "/src/page",
-            service: __dirname + "/src/service",
-            image: __dirname + "/src/image"
+            node_modules: __dirname + '/node_modules',
+            util: __dirname + '/src/util',
+            page: __dirname + '/src/page',
+            service: __dirname + '/src/service',
+            image: __dirname + '/src/image'
         }
     },
+
+    //【新增】：webpack4里面移除了commonChunksPulgin插件，放在了config.optimization里面
+    optimization: {
+        runtimeChunk: false,
+        splitChunks: {
+            cacheGroups: {
+                common: {
+                    name: "common",
+                    chunks: "all",
+                    minChunks: 2
+                }
+            }
+        }
+    },
+
     plugins: [
-        //独立通用模块
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "common",
-            filename: "js/base.js"  //基于output的path
-        }),
-        //css单独打包到文件
-        new ExtractTextPlugin('css/[name].css'),
-        //html模板的处理
-        new HtmlWebpackPlugin(getHtmlConfig("index", '首页')),
-        new HtmlWebpackPlugin(getHtmlConfig("result", '结果页')),
-        new HtmlWebpackPlugin(getHtmlConfig("user-login", '用户登录')),
-        new HtmlWebpackPlugin(getHtmlConfig("user-register", '用户注册')),
-        new HtmlWebpackPlugin(getHtmlConfig("user-pass-reset", '找回密码')),
-        new HtmlWebpackPlugin(getHtmlConfig("user-center", '个人中心')),
-        new HtmlWebpackPlugin(getHtmlConfig("user-center-update", '修改个人信息')),
-        new HtmlWebpackPlugin(getHtmlConfig("detail", '商品详情页')),
-        new HtmlWebpackPlugin(getHtmlConfig("list", '商品清单页')),
-        new HtmlWebpackPlugin(getHtmlConfig("cart", '购物车')),
-        new HtmlWebpackPlugin(getHtmlConfig("user-pass-update", '修改密码')),
-    ]
+        // 【移除】：webpack4里面移除了commonChunksPulgin插件
+        // 独立通用模块到js/base.js
+
+        /*new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        }),*/
+
+        // 把css单独打包到文件里
+        new ExtractTextPlugin("css/[name].css"),
+        // html模板的处理
+        new HtmlWebpackPlugin(getHtmlConfig('index', '首页')),
+        new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表')),
+        new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情')),
+        new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
+        new HtmlWebpackPlugin(getHtmlConfig('payment', '订单支付')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
+        new HtmlWebpackPlugin(getHtmlConfig('about', '关于MMall'))
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8088,
+        inline: true,
+        proxy: {
+            '**/*.do': {
+                target: 'http://test.happymmall.com',
+                changeOrigin: true
+            }
+        }
+    }
 };
 
-if ('dev' === WEBPACK_ENV) {
-    config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
-}
-module.exports = config;
+module.export = config;
